10aa07547e7fe2a58d538448412eead8a0258491
mushfiqur06 <mushfiqur06@gmail.com>
2017-07-11 10:28:52 +0600

    solve 1003.c problem

--- 1003.c
+++ 1003.c
@@ -5,7 +5,7 @@ int main()
     int A, B, SOMA; // Initialize variable
     scanf("%d %d",&A,&B); //Take input
 
-    SOMA = A + B; // A = π . R**2
+    SOMA = A + B;
     printf("SOMA = %d\n",SOMA);
 
     return 0;
