a60a05e19ff75a01baddf5fd6409b266d95af77b
mushfiqur06 <mushfiqur06@gmail.com>
2017-07-11 10:25:47 +0600

    Solve 1003.c problem

---
+++ 1003.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int A, B, SOMA; // Initialize variable
+    scanf("%d %d",&A,&B); //Take input
+
+    SOMA = A + B; // A = π . R**2
+    printf("SOMA = %d\n",SOMA);
+
+    return 0;
+}
+
