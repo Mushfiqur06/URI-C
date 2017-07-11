8934505da3baeb8db00b4a0c224c381396a59cb7
mushfiqur06 <mushfiqur06@gmail.com>
2017-07-11 12:56:08 +0600

    Solve 1004.c problem

---
+++ 1004.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int number1, number2, PROD; // Initialize Variable
+    scanf("%d %d",&number1,&number2); // Take input
+
+    PROD = number1 * number2; // Product of number1 & number2
+    printf("PROD = %d",PROD); // output the PROD
+
+    return 0;
+}
