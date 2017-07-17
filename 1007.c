97e41580f89b17674ece173770fecbaa5200205e
mushfiqur06 <mushfiqur06@gmail.com>
2017-07-17 21:55:58 +0600

    Solve 1007.c Problem

---
+++ 1007.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main() {
+    int A, B, C, D; // Initialize Variable
+    scanf("%d %d %d %d",&A,&B,&C,&D); // Take input
+
+    printf("DIFERENCA = %d",(A * B - C * D)); // Find difference of two product and output that product
+
+    return 0;
+}
